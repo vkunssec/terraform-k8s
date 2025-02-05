c2202e736d91b170a6ce63433fda335f6c34e0ab
Victor Martins <vkunssec@gmail.com>
2025-02-05 10:33:11 -0300

    feat: variables and outputs example

--- main.tf
+++ main.tf
@@ -7,8 +7,14 @@ terraform {
   }
 }
 
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  type        = string
+  default     = "ExampleNginxContainer"
+}
+
 provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
+  host = "npipe:////.//pipe//docker_engine"
 }
 
 resource "docker_image" "nginx" {
@@ -18,10 +24,20 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = var.container_name
 
   ports {
     internal = 80
-    external = 8000
+    external = 8080
   }
+}
+
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = docker_container.nginx.id
+}
+
+output "image_id" {
+  description = "ID of the Docker image"
+  value       = docker_image.nginx.id
 }
